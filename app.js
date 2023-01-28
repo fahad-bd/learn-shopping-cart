80e90fb4745bfcdd55159e18c6fdd20041649e32
Fahad <fahadahammed1@gmail.com>
2023-01-28 15:04:08 +0600

    add js for increase and decrease case number

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // console.log('js connected');
+
+
+
 document.getElementById('case-plus').addEventListener('click', function(){
     // console.log('click in case pluse');
     const caseInput = document.getElementById('case-number');
     const caseNumber = caseInput.value;
     // console.log(caseNumber);
     caseInput.value = parseInt(caseNumber) + 1;
+});
+
+document.getElementById('case-minus').addEventListener('click', function(){
+    const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) - 1;
 });
