954e64fa16a962830518e4f028b6e336b1166d8f
Fahad <fahadahammed1@gmail.com>
2023-01-28 15:17:02 +0600

    update balance

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 // console.log('js connected');
 
-function updateCaseNumber(){
+function updateCaseNumber(isIncreasing){
     const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) + 1;
+    let caseNumber = caseInput.value;
+    if(isIncreasing)
+    {
+        caseInput.value = parseInt(caseNumber) + 1;
+        caseNumber++;
+    }    
+    else if(caseNumber > 0)
+    {
+        caseInput.value = parseInt(caseNumber) - 1;   
+        caseNumber--;
+    }    
+        
+    //update case total
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 59;
 }
 
 document.getElementById('case-plus').addEventListener('click', function(){
-    updateCaseNumber();
+    updateCaseNumber(true);
 });
 
 document.getElementById('case-minus').addEventListener('click', function(){
-    const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) - 1;
+    updateCaseNumber(false);
 });
