6eeebc60f44f4a34a5f55817911796a1d4afab72
Fahad <fahadahammed1@gmail.com>
2023-01-28 17:43:14 +0600

    add total price with tax

--- app.js
+++ app.js
@@ -1,38 +1,62 @@
 // console.log('js connected');
 
-function updateCaseNumber(product, price, isIncreasing){
-    const caseInput = document.getElementById(product + '-number');
-    let caseNumber = caseInput.value;
+function updateProductNumber(product, price, isIncreasing){
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
     if(isIncreasing)
     {
-        caseNumber = parseInt(caseNumber) + 1;
+        productNumber = parseInt(productNumber) + 1;
     }    
-    else if(caseNumber > 0)
+    else if(productNumber > 0)
     {
-        caseNumber = parseInt(caseNumber) - 1;   
+        productNumber = parseInt(productNumber) - 1;   
     }    
         
-    caseInput.value = caseNumber;
+    productInput.value = productNumber;
     //update case total
-    const caseTotal = document.getElementById(product + '-total');
-    caseTotal.innerText = caseNumber * price;
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
+
+    //calculate total
+    calculateTotal();
+}
+
+function getInputValue(product)
+{
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+
+function calculateTotal()
+{
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+
+    const textAmonunt = subTotal / 10;
+    const totalPrice = subTotal + textAmonunt;
+
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = textAmonunt;
+    document.getElementById('total-price').innerText = totalPrice;
 }
 
 //phone increase decrease event
 document.getElementById('phone-plus').addEventListener('click', function(){
     // console.log('phone plus clicked');
-    updateCaseNumber('phone', 1219, true);
+    updateProductNumber('phone', 1219, true);
 });
 document.getElementById('phone-minus').addEventListener('click', function(){
     // console.log('phone plus clicked');
-    updateCaseNumber('phone', 1219, false);
+    updateProductNumber('phone', 1219, false);
 });
 
 //case increase decrease event
 document.getElementById('case-plus').addEventListener('click', function(){
-    updateCaseNumber('case', 59, true);
+    updateProductNumber('case', 59, true);
 });
 
 document.getElementById('case-minus').addEventListener('click', function(){
-    updateCaseNumber('case', 59, false);
+    updateProductNumber('case', 59, false);
 });
